39cb62dd824f00daf6a3c564478a835937efbb77
Yu Li <yli0607x@gmail.com>
2018-10-04 13:14:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
+def countdown(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
     sleep(1)
-    seconds_to_midnight -= 1
+    i -= 1
   end
   "HAPPY NEW YEAR!"
 end
